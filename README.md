7bd38043b20bdcf0c3725ba87b5c80c2706c932b
Damien GILLES <pro@gillesdamien.com>
2025-07-13 07:53:13 +0200

    cleanup

--- README.md
+++ README.md
@@ -31,23 +31,8 @@ Run the sync script locally:
 ```bash
 npm start          # Run original sync script
 npm run sync       # Run enhanced sync with better logging
-node verify-setup.js  # Verify GitHub Actions setup
 ```
 
 ## The model
 
 The tables of the database are defined in `db\genTables.sql`. The database can initially be generated with the script `db\genTables.js`.
-
-## Information on the site to scrap
-
-- yugioh db features the following locales refered as lang in the database: "en", "fr", "ja", "de", "ae", "cn", "es", "it", "ko", "pt".
-- yugioh db assign an ID to each card, the first card ID is 4007.
-- some cards do not exist in every locales, the db is fully scrapped if for a given id, no pages exist in any of the locales. If a card does not exist in a given local no localization table entry is created for that card locale.
-- The url of a card with id `ID` in locale `LOC` is `https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid=ID&request_locale=LOC`.
-- The table edition exist beceause a card may be edited in multiple cardset and multiple times in a cardset under different rarities. If the rarity of a card cannot be found it is considered as `C` for common.
-- Translations for the monsters Attribute, Icon, Monster Type and Card Type can be obtained by scrapping the search form for each locale at `https://www.db.yugioh-card.com/yugiohdb/card_search.action?wname=CardSearch&request_locale=LOC` by replacing LOC by one of the locale.
-- Some language may contain monster type that other language does not have, if the monster type exist in english, the english translation should be used in the database else the japanese translation should be used. If an english translation is added later for a monster type in yugiohdb the db should be updated to use that english translation.
-- The url `https://www.db.yugioh-card.com/yugiohdb/card_list.action?request_locale=LOC` list the different cardset published for a given locale.
-- The url `https://www.db.yugioh-card.com/yugiohdb/forbidden_limited.action?forbiddenLimitedDate=DATE&request_locale=LOC` shows the cards that have limitation at a given date for a given locale, such list is called a banlist.
-- The region of a banlist is `TCG` for every locale exept for japanese "ja" which is `OCG`.
-- Any error occuring during the execution should be logged, if the error concerns a card, a card set or a banlist we should skip it and process the next one.
--- db/genTables.sql
+++ db/genTables.sql
@@ -1,21 +1,22 @@
+-- Language abbreviation (e.g., 'en', 'fr')
 CREATE TABLE lang(
     id INTEGER PRIMARY KEY,
-    abbr TEXT NOT NULL UNIQUE -- ex: fr
+    abbr TEXT NOT NULL UNIQUE
 );
 
--- card informations
-
+-- monster types in english if it exists (e.g., Dragon, Warrior)
 CREATE TABLE monster_type(
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE -- english card type if the card type exist in english else in japanese, ex: Dragon
+    name TEXT NOT NULL UNIQUE
 );
 
+-- Card types (Normal,Effect,Fusion,Flip,Special summon,Ritual,Toon,Spirit,Union,Gemini,Synchro,Tuner,Xyz,Pendulum,Link)
 CREATE TABLE type(
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE -- english card type, ex: Cyberse
+    name TEXT NOT NULL UNIQUE 
 );
 
----- a table to join 1 card to many types
+-- Associates cards with their respective types (many-to-many relationship)
 CREATE TABLE type_card(
     type_id INTEGER,
     card_id INTEGER,
@@ -25,51 +26,56 @@ CREATE TABLE type_card(
     FOREIGN KEY(card_id) REFERENCES card(id)
 );
 
+-- Card attributes (LIGHT,EARTH,WIND,DARK,FIRE,WATER,DIVINE,Equip Spell,Normal Spell,Continuous Trap,Field Spell,Normal Trap,Ritual Spell,Continuous Spell,Counter Trap,Quick-Play Spell)
 CREATE TABLE attribute(
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE -- english card attribute ex: light, continuousspell, etc...
+    name TEXT NOT NULL UNIQUE
 );
 
--- this table describle the card informations that are subject to change depending on the locale
+-- Stores localized (translated) information for cards.
 CREATE TABLE localization(
     id INTEGER PRIMARY KEY,
     card_id INTEGER,
     lang_id INTEGER,
-    name TEXT, -- ex: Fiendish Chain
-    card_text TEXT, -- ex: Draw 2 cards.
-    pendulum_effect TEXT, -- null if not concerned
+    name TEXT,
+    card_text TEXT,
+    pendulum_effect TEXT,
 
     FOREIGN KEY(card_id) REFERENCES card(id),
     FOREIGN KEY(lang_id) REFERENCES lang(id)
 );
 
+-- Central table containing core card data
 CREATE TABLE card(
-    id INTEGER PRIMARY KEY, -- yugioh db.yugioh-card id
-    monster_type_id INTEGER, -- monster type or null if not concerned
-    attribute_id INTEGER, -- card attribute
-    -- level_rank_arrows is a flag for link monster, the flag is computed from 
+    id INTEGER PRIMARY KEY, -- yugioh db.yugioh-card.com card id
+    monster_type_id INTEGER, -- monster type id or null if the card is not a monster
+    attribute_id INTEGER,
+    -- level of the card if it is a non xyz, non link monster
+    -- rank of the card if it is a xyz monster
+    -- If this is a link monster level_rank_arrows is a flag for link monster, the flag is computed from 
     -- the numpads position of the arrows with the formula
     -- SUM(2^(x-1)) where x is every number on the numpad that match with a link arrow
-    level_rank_arrows INTEGER, -- null if not concerned
-    atk INTEGER, -- null if "?" or not concerned
-    def INTEGER, -- null if "?" or not concerned
-    pendulum_scale, -- null if not concerned
+    -- ex: bottom right+top arrow = 2^(3-1) + 2^(8-1) = 4 + 128 = 132
+    level_rank_arrows INTEGER, -- null if not a monster
+    atk INTEGER, -- null if atk is the special value "?" or if it is not a monster
+    def INTEGER, -- null if def is the special value "?" or if it is not a monster
+    pendulum_scale INTEGER, -- null if it is not a pendulum monster
 
     FOREIGN KEY(monster_type_id) REFERENCES monster_type(id),
     FOREIGN KEY(attribute_id) REFERENCES attribute(id)
 );
 
--- sets infomations
-
+-- A set of cards, usually a booster pack or structure deck
 CREATE TABLE cardset(
     id INTEGER PRIMARY KEY, -- yugioh db.yugioh-card pack id (pid)
     name TEXT, -- ex: DESTINY SOLDIERS
-    release_date DATE,
+    release_date DATE, -- ex: 2016-04-16 maybe null if not known
     lang_id INTEGER,
 
     FOREIGN KEY(lang_id) REFERENCES lang(id)
 );
 
+-- Rarity of a card
 CREATE TABLE rarity(
     name TEXT NOT NULL, -- ex: SR
     long_name TEXT NOT NULL, -- ex: Super Rare
@@ -77,7 +83,7 @@ CREATE TABLE rarity(
     PRIMARY KEY (name, long_name)
 );
 
-
+-- A card can have multiple editions in the same or different cardset, each edition have a rarity
 CREATE TABLE edition_rarity(
     edition_rowid INTEGER,
     rarity_rowid INTEGER,
@@ -87,32 +93,34 @@ CREATE TABLE edition_rarity(
     FOREIGN KEY(rarity_rowid) REFERENCES rarity(rowid)
 );
 
+-- A card can have multiple editions in the same or different cardset, each edition have a card number
 CREATE TABLE edition(
-    card_id INTEGER, -- REFERENCES card(id)
+    card_id INTEGER,
     cardset_id INTEGER,
-    card_number TEXT NOT NULL, -- ex: AP08-EN001
+    card_number TEXT NOT NULL, -- ex: AP08-EN001, null for some cards
 
     PRIMARY KEY (card_id, cardset_id, card_number),
     FOREIGN KEY(card_id) REFERENCES card(id),
     FOREIGN KEY(cardset_id) REFERENCES cardset(id)
 );
 
--- ban list informations
-
+-- The maximum number of card allowed in a deck, side deck or extra deck
 CREATE TABLE limitation(
     banlist_id INTEGER,
-    card_id INTEGER, -- REFERENCES card(id)
+    card_id INTEGER,
     limitation INTEGER, -- ex: 0 = forbidden, 1 = limited, 2 = semi_limited
 
     PRIMARY KEY (banlist_id, card_id),
     FOREIGN KEY(banlist_id) REFERENCES banlist(id)
 );
 
+-- A region of a banlist, ex: TCG (Trading Card Game), OCG (Official Card Game). OCG is for cards in ja, ko and ae; TCG is for cards in en, fr, de, it, es and pt.
 CREATE TABLE region(
     id INTEGER PRIMARY KEY,
     region TEXT NOT NULL UNIQUE -- TCG, OCG
 );
 
+-- A banlist is a list of cards that are limited, semi-limited or forbidden in a specific region from a date and until another banlist is released
 CREATE TABLE banlist(
     id INTEGER PRIMARY KEY,
     region_id INTEGER,
